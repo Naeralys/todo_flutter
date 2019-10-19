964c461f06e438fb6343a763ca0f0bce51ac68e0
Adam Lind <adamlind@live.se>
2019-10-19 13:45:19 +1100

    initial backend project, todolist controller and first get route

---
+++ backend/Controllers/TodoListController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("api/TodoList")]
+    public class TodoListController : ControllerBase
+    {
+        private static readonly List<TodoItem> todos = new List<TodoItem>() {new TodoItem("A simple todo")};
+
+        [HttpGet]
+        public List<TodoItem> Get() => todos;
+
+        [HttpGet("{index}")]
+        public TodoItem GetFirst(int index) => todos[index];
+
+        [HttpPost]
+        public IActionResult Add(string name) {
+            return Content(name);
+        }
+    }
+}
---
+++ backend/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace backend.Models
+{
+    public class TodoItem
+    {
+        private string _name;
+        public TodoItem(string name)
+        {
+            _name = name;
+        }
+        public string Name { get => _name; set => _name = value; }
+    }
+}
